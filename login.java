dffa671395172871058368724877411a28a228a6
vedantmurade13-cpu <vedantmurade13@gmail.com>
2026-02-13 21:00:10 +0530

    Add files via upload

---
+++ login.java
@@ -0,0 +1,17 @@
+package employee.manage.system;
+import javax.swing.*;
+import java.awt.*;
+
+public class login extends JFrame{
+    login(){
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        setSize(600,300);
+        setLocation(450,200);
+        setVisible(true);
+    }
+    public static void main(String args[]){
+        new login();
+    }
+    
+}
