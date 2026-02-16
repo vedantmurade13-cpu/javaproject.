1887e88004efbd9f6bc946c78747c153fc6eff10
vedantmurade13-cpu <vedantmurade13@gmail.com>
2026-02-16 22:43:52 +0530

    Update login.java

--- login.java
+++ login.java
@@ -1,28 +1,34 @@
 package employee.manage.system;
 import javax.swing.*;
 import java.awt.*;
-
+//used here to create a login for info of employees                        
 public class login extends JFrame{
     login(){
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
+        
+        //user name label
+        
         JLabel askusername=new JLabel("USERNAME");
         askusername.setBounds(40,20,100,20);
         add(askusername);
         
-        
+        //username textfeild
         JTextField tfusername=new JTextField();
         tfusername.setBounds(150,20,150,20);
         add(tfusername);
-        
-        
+
+        //labelling field of password 
         JLabel passwords=new JLabel("PASSWORD");
         passwords.setBounds(40,40,100,20);
         add(passwords);
-        
+
+        //text field of password
          JTextField passkey=new JTextField();
         passkey.setBounds(150,40,150,20);
         add(passkey);
+        
+        //code of login button
         
         JButton testlogin = new JButton("LOGIN");
         testlogin.setBounds(150,60,150,30);
@@ -39,5 +45,6 @@ public class login extends JFrame{
     }
     
 }
+
 
 
